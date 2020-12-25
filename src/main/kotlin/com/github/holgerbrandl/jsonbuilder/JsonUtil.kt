8c16d03d10ed9753064e7eafa44f0aedbe450b36
Anthony <576946+ngwing@users.noreply.github.com>
2020-12-25 16:18:59 +0800

    Update JsonUtil.kt

--- src/main/kotlin/com/github/holgerbrandl/jsonbuilder/JsonUtil.kt
+++ src/main/kotlin/com/github/holgerbrandl/jsonbuilder/JsonUtil.kt
@@ -4,10 +4,6 @@ import org.json.JSONArray
 import org.json.JSONObject
 import java.util.*
 
-
-//Source: https://stackoverflow.com/questions/41861449/kotlin-dsl-for-creating-json-objects-without-creating-garbage
-
-
 fun json(build: JsonObjectBuilder.() -> Unit): JSONObject {
     return JsonObjectBuilder().json(build)
 }
@@ -31,24 +27,4 @@ class JsonObjectBuilder {
 
         deque.peek().put(this, wrapped)
     }
-
-//    infix fun <T> String?.to(value: T) {
-//        if (this == null) {
-//            val wrapped = when (value) {
-//                is Array<*> -> deque.peek().put(this, JSONArray().apply { value.forEach { put(it) } })
-//                else -> TODO("not support. Please file an issue if you that this is an error of API")
-//            }
-//
-//        } else {
-//            // wrap value into json block if it is a lambda
-//            val wrapped = when (value) {
-//                is Function0<*> -> json { value.invoke() }
-//                is Array<*> -> JSONArray().apply { value.forEach { put(it) } }
-//                else -> value
-//            }
-//
-//            deque.peek().put(this, wrapped)
-//
-//        }
-//    }
 }
